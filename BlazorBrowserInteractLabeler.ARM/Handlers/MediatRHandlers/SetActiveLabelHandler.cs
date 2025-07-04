bf64ac97044246b896dc07a44f51684f8751f832
alex <monteristo1946@gmail.cim>
2025-07-04 13:58:58 +0300

    реализовал изменение цвета активного

--- BlazorBrowserInteractLabeler.ARM/Handlers/MediatRHandlers/SetActiveLabelHandler.cs
+++ BlazorBrowserInteractLabeler.ARM/Handlers/MediatRHandlers/SetActiveLabelHandler.cs
@@ -13,10 +13,12 @@ public class SetActiveLabelHandler : IRequestHandler<SetActiveLabelQueries, bool
 {
     private readonly ILogger _logger = Log.ForContext<SetActiveLabelHandler>();
     private readonly MarkupData _markupData;
+    private readonly AnnotationHandler _annotationHandler;
 
-    public SetActiveLabelHandler(MarkupData markupData)
+    public SetActiveLabelHandler(MarkupData markupData, AnnotationHandler annotationHandler)
     {
         _markupData = markupData ?? throw new ArgumentNullException(nameof(markupData));
+        _annotationHandler = annotationHandler ?? throw new ArgumentNullException(nameof(annotationHandler));
     }
 
     public async Task<bool> Handle(SetActiveLabelQueries? request, CancellationToken cancellationToken)
@@ -28,14 +30,38 @@ public class SetActiveLabelHandler : IRequestHandler<SetActiveLabelQueries, bool
 
             _markupData.CurrentLabelId = request.IdLabel;
 
+            var allAnnots = await _annotationHandler.GetAllAnnotations();
+
+            if (allAnnots.Any() is false)
+                return false;
+
+            await SetIdLabelInActiveAnnot(allAnnots, _markupData.CurrentLabelId);
+
             return true;
         }
         catch (Exception e)
         {
             _logger.Error("[HiddenAnnotHandler] {@Exception}", e);
         }
-
-
+        
         return false;
+    }
+
+    private async Task SetIdLabelInActiveAnnot(Annotation[] allAnnots, int labelId)
+    {
+        var isUpdate = false;
+        foreach (var annotation in allAnnots)
+        {
+            if (annotation.State != StateAnnot.Edit)
+                continue;
+
+            annotation.LabelId = labelId;
+            isUpdate = true;
+        }
+
+        if (isUpdate)
+        {
+            await _annotationHandler.UpdateAllAnnotations(allAnnots);
+        }
     }
 }
